1ccb524e5bd473c05ccdca6df0c9aa365c6347b2
Fabian Morales Astorga <fmorales.astorga.15@gmail.com>
2022-08-02 13:47:12 -0400

    update

---
+++ JavaScriptBasico/1.2/index.js
@@ -0,0 +1,11 @@
+lista = {
+	name: "Fabian",
+	edad: 26,
+	"Dev?": True,
+	"Fecha de nacimiento": new Date(10 - 10 - 1995),
+	favBook: {
+		title: "Nacidos de la bruma",
+		Author: "Brandon Sanderson",
+		Year: 2007,
+	},
+};
